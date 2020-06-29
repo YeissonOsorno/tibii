466f8b3c1ba709c13879332d536ec443d94b931d
BrahianCano <brahianestiven08@gmail.com>
2020-06-28 20:44:06 -0500

    Se mejoro la experiencia de usuario al registrar un cliente

--- view/src/pages/client/Sigin/Sigin.jsx
+++ view/src/pages/client/Sigin/Sigin.jsx
@@ -3,6 +3,9 @@ import React, { Fragment, useState, useEffect } from 'react';
 // Importacion de estilos //
 import './sigin.css'
 
+// Importacion de imagenes //
+import shop from '../../../assets/images/shop.svg';
+
 // Importacion de componentes //
 import NavBar from '../../../components/Navbar/index';
 import FormClient from '../../../components/FormClient/index';
@@ -36,9 +39,10 @@ export default function Sigin() {
             <main id="cliente-login">
                 <div className="row">
                     <div className="col s12 m5 l5 xl5">
-                        <h3>Bienvenido</h3>
-                        <h5>tibii es el lugar perfecto para tu negocio.</h5>
-                        <ul className="mt30 left-align">
+                        <img className="responsive-img" src={shop} alt="shop" />
+                        <h3>Bienvenido a tibii</h3>
+                        <h5>Este es el lugar perfecto para tu negocio.</h5>
+                        <ul className="mt10 left-align">
                             <li><span className="material-icons left">supervisor_account</span>Lleva un control de tus clientes.</li>
                             <li><span className="material-icons left">store</span>Controla el flujo de personas en tu tienda.</li>
                             <li><span className="material-icons left">local_mall</span>Gestiona productos y encargos.</li>
@@ -46,10 +50,17 @@ export default function Sigin() {
                         </ul>
                     </div>
                     <div className="col s12 m7 l7 xl7">
-                        <h3>Crea tu cuenta</h3>
                         {!toggleForm ?
-                            <FormClient onSubmitClient={onSubmitClient} /> :
-                            <FormCompany onSubmitCompany={onSubmitCompany} toggleForm={toggleForm}/>
+                            <>
+                                <h3>Crea tu cuenta</h3>
+                                <p className="container grey-text text-darken-2 pt10">Registra tus datos como dueño del negocio, esta información sera de utilidad para accerder al panel de control.</p>
+                                <FormClient onSubmitClient={onSubmitClient} />
+                            </> :
+                            <>
+                                <h3>Datos del negocio</h3>
+                                <p className="container grey-text text-darken-2 pt10">Registra los datos acerca de tu negocio, esta informacion sera vizta por las personas que deseen comprar tus productos.</p>
+                                <FormCompany onSubmitCompany={onSubmitCompany} toggleForm={toggleForm} />
+                            </>
                         }
                     </div>
                 </div>
